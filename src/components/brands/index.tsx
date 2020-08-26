5cdc1f93bb56369b5e29de7c472569f59015f0de
bora <boraoren@gmail.com>
2020-08-26 11:51:01 +1000

    feat(page2/components/brands): implement brands draft panel

---
+++ src/components/brands/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Brands = () => {
+    return(
+        <View>
+            <Text>
+                Brands
+            </Text>
+        </View>
+    )
+}
+
+export default Brands;
--- src/screens/ScreenPage2.tsx
+++ src/screens/ScreenPage2.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, SafeAreaView, StatusBar } from 'react-native';
 import PageHeader from '../components/page/header';
 import ApplicationName from '../components/label/applicationname';
+import Brands from '../components/brands';
 
 const ScreenPage2 = () =>{
     return(
@@ -9,6 +10,7 @@ const ScreenPage2 = () =>{
             <StatusBar barStyle="dark-content"/>
             <PageHeader menuDrawerColor={'#000'}/>
             <ApplicationName/>
+            <Brands/>
         </SafeAreaView>
     )
 }
