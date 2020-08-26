29732903f715bbeb9e05065a07885b3cb4555b8e
bora <boraoren@gmail.com>
2020-08-26 11:20:41 +1000

    feat(page2/screen): implement draft screen page 2

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import ScreenPage1 from './src/screens/ScreenPage1';
+import ScreenPage2 from './src/screens/ScreenPage2';
 
-AppRegistry.registerComponent(appName, () => ScreenPage1);
+AppRegistry.registerComponent(appName, () => ScreenPage2);
---
+++ src/screens/ScreenPage2.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, SafeAreaView, StatusBar } from 'react-native';
+
+const ScreenPage2 = () =>{
+    return(
+        <SafeAreaView>
+            <StatusBar barStyle="dark-content"/>
+            <Text>Screen Page 2</Text>
+        </SafeAreaView>
+    )
+}
+
+export default ScreenPage2;
