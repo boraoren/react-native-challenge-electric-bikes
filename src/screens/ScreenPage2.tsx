5b6603a68b49b614a8c3ff041ba1306a742769cd
bora <boraoren@gmail.com>
2020-08-28 14:42:56 +1000

    feat(page2/backgroundimage): add thunder background image for page 2

--- src/screens/ScreenPage2.tsx
+++ src/screens/ScreenPage2.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { View, Text, SafeAreaView, StatusBar } from 'react-native';
+import { View, Text, SafeAreaView, StatusBar, ImageBackground, StyleSheet } from 'react-native';
 import PageHeader from '../components/page/header';
 import ApplicationName from '../components/label/applicationname';
 import Brands from '../components/brands';
 import PageBottomMenu from '../components/page/bottom/menu';
 
 const ScreenPage2 = () =>{
+
+    const screenPage2BackgroundImage = 
+        require('./screenPage2Background.png');
+
     return(
         <SafeAreaView>
             <StatusBar barStyle="dark-content"/>
+            <ImageBackground
+                source={screenPage2BackgroundImage}
+                style={styles.screenPage2BackgroundImage}
+            />
             <PageHeader menuDrawerColor={'#000'}/>
             <ApplicationName/>
             <Brands/>
@@ -16,5 +24,13 @@ const ScreenPage2 = () =>{
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    screenPage2BackgroundImage: {
+        width: '100%',
+        height: 600,
+        position: 'absolute',
+    }
+})
 
 export default ScreenPage2;
