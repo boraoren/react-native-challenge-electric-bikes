995761f7da6070f6e397f5f91ccd4c8f4d71cff2
bora <boraoren@gmail.com>
2020-09-02 13:52:09 +1000

    feat(page3/header): implement header

--- src/screens/ScreenPage3.tsx
+++ src/screens/ScreenPage3.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { View, StyleSheet, SafeAreaView, StatusBar, Text } from 'react-native';
+import PageHeader from '../components/page/header';
 
 const ScreenPage3 = () => {
     return(
         <SafeAreaView style={styles.screenPage3}>
             <StatusBar barStyle="light-content"/>
-            <Text style={styles.text}>
-                Screen Page 3
-            </Text>
+            <PageHeader menuDrawerColor={'#FAE957'}/>
         </SafeAreaView>
     )
 }
